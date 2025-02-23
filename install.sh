7ff0d1941b578b4e3f01a51a466cb9711e754272
maxuwuu <emirshn000@gmail.com>
2025-02-23 13:42:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ git clone https://github.com/maxuwuu/.dotfiles.git ~/dotfiles
 
 
 if command -v apt &> /dev/null; then
-    sudo apt update && sudo apt install -y i3 dmenu polybar feh dolphin konsole  
+    sudo apt update && sudo apt install -y i3 dmenu polybar feh dolphin konsole firefox neofetch 
 elif command -v pacman &> /dev/null; then
-    sudo pacman -Syu --noconfirm i3 dmenu polybar feh dolphin konsole
+    sudo pacman -Syu --noconfirm i3 dmenu polybar feh dolphin konsole firefox neofetch 
 elif command -v dnf &> /dev/null; then
-    sudo dnf install -y i3 dmenu polybar feh dolphin konsole
+    sudo dnf install -y i3 dmenu polybar feh dolphin konsole firefox screenfetch 
 elif command -v zypper &> /dev/null; then
     sudo zypper install -y i3 dmenu polybar feh dolphin konsole
 else
