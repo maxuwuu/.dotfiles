a8aaf91c2ef0f1aff507faf2f63fbbc0934b0b9b
maxuwuu <emirshn000@gmail.com>
2025-02-23 17:44:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ git clone https://github.com/maxuwuu/.dotfiles.git ~/dotfiles
 
 
 if command -v apt &> /dev/null; then
-    sudo apt update && sudo apt install -y i3 dmenu polybar feh dolphin konsole firefox neofetch 
+    sudo apt update && sudo apt install -y i3 dmenu polybar feh dolphin konsole firefox neofetch picom
 elif command -v pacman &> /dev/null; then
-    sudo pacman -Syu --noconfirm i3 dmenu polybar feh dolphin konsole firefox neofetch 
+    sudo pacman -Syu --noconfirm i3 dmenu polybar feh dolphin konsole firefox neofetch picom
 elif command -v dnf &> /dev/null; then
-    sudo dnf install -y i3 dmenu polybar feh dolphin konsole firefox screenfetch 
+    sudo dnf install -y i3 dmenu polybar feh dolphin konsole firefox screenfetch picom 
 elif command -v zypper &> /dev/null; then
-    sudo zypper install -y i3 dmenu polybar feh dolphin konsole
+    sudo zypper install -y i3 dmenu polybar feh dolphin konsole firefox screenfetch picom 
 else
     echo "Unsupported distribution!"
     exit 1
