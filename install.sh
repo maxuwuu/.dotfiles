914a4baedb7afaa092f08673536aebb09e0976b4
Max <emirshn000@gmail.com>
2025-03-01 15:04:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git clone https://github.com/maxuwuu/.dotfiles.git ~/dotfiles
 
 
 if command -v apt &> /dev/null; then
-    sudo apt update && sudo apt install -y i3 dmenu polybar feh dolphin konsole firefox neofetch picom sddm
+    sudo apt update && sudo apt install -y i3 dmenu polybar feh dolphin konsole firefox neofetch picom sddm 
 elif command -v pacman &> /dev/null; then
     sudo pacman -Syu --noconfirm i3 dmenu polybar feh dolphin konsole firefox neofetch picom sddm 
 elif command -v dnf &> /dev/null; then
@@ -21,7 +21,7 @@ fi
 mkdir -p ~/.config
 ln -sf ~/dotfiles/config/i3 ~/.config/i3
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-
+ln -sf ~/dotfiles/polybar ~/.config/polybar
 echo "Installation completed. Please restart your session or reload i3."
 
 
