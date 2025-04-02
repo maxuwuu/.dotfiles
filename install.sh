7bc9887d8c9e0852af6d7bce14a5c13b8b608830
Max <emirshn000@gmail.com>
2025-04-02 23:26:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,25 +5,36 @@ DOTFILES_DIR="$HOME/.dotfiles"
 CONFIG_DIR="$HOME/.config"
 LINUX_DIR="$DOTFILES_DIR/linux"
 
-HOME_FILES=(".nanorc" ".gitconfig")
-CONFIG_FILES=("i3" "rofi" "polybar" "neofetch" "nitrogen")
-PACKAGES=("bash" "neofetch" "nano" "git" "i3" "rofi" "polybar" "nitrogen" "feh")
+HOME_FILES=(".nanorc" ".gitconfig" ".bashrc")
+CONFIG_FILES=("i3" "rofi" "polybar" "neofetch" "nitrogen" "dunst")
+PACKAGES_ARCH=("alacritty" "picom" "rofi" "polybar" "firefox" "flatpak" "snapd" "gnome-software" "dunst")
+PACKAGES_UBUNTU=("alacritty" "picom" "rofi" "polybar" "firefox" "gnome-software" "flatpak" "snapd" "dunst")
+PACKAGES_FEDORA=("alacritty" "picom" "rofi" "polybar" "firefox" "gnome-software" "flatpak" "snapd" "dunst")
 
 install_packages() {
-    if command -v apt &>/dev/null; then
-        sudo apt update && sudo apt install -y "${PACKAGES[@]}"
+    if command -v pacman &>/dev/null; then
+        sudo pacman -Syu --noconfirm "${PACKAGES_ARCH[@]}"
+    elif command -v apt &>/dev/null; then
+        sudo apt update && sudo apt install -y "${PACKAGES_UBUNTU[@]}"
     elif command -v dnf &>/dev/null; then
-        sudo dnf install -y "${PACKAGES[@]}"
-    elif command -v pacman &>/dev/null; then
-        sudo pacman -Syu --noconfirm "${PACKAGES[@]}"
+        sudo dnf install -y "${PACKAGES_FEDORA[@]}"
     else
         exit 1
     fi
 }
 
-read -p "Do you want to install dotfiles and required packages? (yes/no): " answer
+backup_and_symlink() {
+    local file_or_dir="$1"
+    local target="$2"
+    if [ -e "$target" ] || [ -L "$target" ]; then
+        mv "$target" "${target}.bak"
+    fi
+    ln -s "$file_or_dir" "$target"
+}
 
-if [[ "$answer" == "yes" ]]; then
+read -p "Do you want to install dotfiles and required packages? (y/n): " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
     install_packages
 
     if [ -d "$DOTFILES_DIR" ]; then
@@ -35,24 +46,15 @@ if [[ "$answer" == "yes" ]]; then
     mkdir -p "$CONFIG_DIR"
 
     for file in "${HOME_FILES[@]}"; do
-        if [ -f "$HOME/$file" ] || [ -L "$HOME/$file" ]; then
-            mv "$HOME/$file" "$HOME/${file}.bak"
-        fi
-        ln -s "$DOTFILES_DIR/$file" "$HOME/$file"
+        backup_and_symlink "$DOTFILES_DIR/$file" "$HOME/$file"
     done
 
     if [ -f "$LINUX_DIR/.bashrc" ]; then
-        if [ -f "$HOME/.bashrc" ] || [ -L "$HOME/.bashrc" ]; then
-            mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
-        fi
-        ln -s "$LINUX_DIR/.bashrc" "$HOME/.bashrc"
+        backup_and_symlink "$LINUX_DIR/.bashrc" "$HOME/.bashrc"
     fi
 
     for dir in "${CONFIG_FILES[@]}"; do
-        if [ -d "$CONFIG_DIR/$dir" ] || [ -L "$CONFIG_DIR/$dir" ]; then
-            mv "$CONFIG_DIR/$dir" "$CONFIG_DIR/${dir}.bak"
-        fi
-        ln -s "$DOTFILES_DIR/$dir" "$CONFIG_DIR/$dir"
+        backup_and_symlink "$DOTFILES_DIR/$dir" "$CONFIG_DIR/$dir"
     done
 
     echo "Installation complete!"
